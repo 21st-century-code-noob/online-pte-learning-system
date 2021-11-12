0e32176f7efe3cd38329107931e20166f378ad95
Yumikoo <huyuming1995@gmail.com>
2021-11-12 19:51:45 +1100

    Router adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import DashboardPage from "pages/dashboard/dashboardPage"
-import Hero from "pages/landing-page/components/Hero/Hero"
-import { PracticeArea } from "pages/landing-page/components/PracticeArea/PracticeArea"
-import StudyMaterial from "pages/landing-page/components/StudyMaterial/StudyMaterial"
-import Testimonial from "pages/landing-page/components/Testimonial/Testimonial"
+import LandingPage from "pages/landing-page/LandingPage"
 import Login from "pages/login-and-signup/login-page/Login"
 import Signedup from "pages/login-and-signup/signup-page/Signedup"
 import Signup from "pages/login-and-signup/signup-page/Signup"
-import Footer from "pages/public-component/Footer/Footer"
+import OnlinePractice from "pages/practicing/PracticeHomepage/OnlinePractice"
+import RAList from "pages/practicing/QuestionType/RA/QuestionList/RAList"
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { createGlobalStyle } from "styled-components"
 import theme from "styles/theme"
-
-import Navbar from "./pages/public-component/Navbar/Navbar"
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
@@ -24,30 +20,19 @@ const GlobalStyles = createGlobalStyle`
   }
 `
 
-const Homepage = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <PracticeArea />
-      <StudyMaterial />
-      <Testimonial />
-      <Footer />
-    </>
-  )
-}
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyles />
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signedup" element={<Signedup />} />
           <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/practice" element={<OnlinePractice />} />
+          <Route path="/practice/Speaking/RA" element={<RAList />} />
         </Routes>
       </Router>
     </ThemeProvider>
